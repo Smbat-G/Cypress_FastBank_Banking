9586a2ddec022ec7d8785888ad6b8b726b8d3bf6
Smbat Grigoryan <smbat.grigoryan@org.fcc.am>
2026-05-12 16:34:20 +0400

    updateted test file

--- cypress/e2e/POM/tests/login.cy.js
+++ cypress/e2e/POM/tests/login.cy.js
@@ -2,46 +2,45 @@ import loginPage from '../pages/loginPage'
 
 describe('Login Page Tests', () => {
 
-beforeEach(() => {
-  cy.clearAllSessionStorage()
-  cy.clearCookies()
-  cy.clearLocalStorage()
-  Cypress.session.clearAllSavedSessions()
-  cy.visit('/auth/login')
-})
+  beforeEach(() => {
+    cy.clearAllSessionStorage()
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    Cypress.session.clearAllSavedSessions()
+    cy.visit('/auth/login')
+  })
 
-it.only('should login with valid credentials', () => {
-  cy.login(Cypress.env('username'), Cypress.env('password'))
-  cy.url().should('not.include', 'auth/login')
-})
+  it('should login with valid credentials', () => {
+    cy.login(Cypress.env('username'), Cypress.env('password'))
+    cy.url().should('not.include', 'auth/login')
+  })
 
   it('should not login with invalid username', () => {
-    cy.login('wronguser', Cypress.env('password'))
-    cy.visit('/auth/login')
+    loginPage.typeUsername('wronguser')
+    loginPage.typePassword(Cypress.env('password'))
+    loginPage.clickLogin()
     loginPage.verifyLoginFailed()
   })
 
   it('should not login with invalid password', () => {
-    cy.login(Cypress.env('username'), 'wrongpass')
-    cy.visit('/auth/login')
+    loginPage.typeUsername(Cypress.env('username'))
+    loginPage.typePassword('wrongpass')
+    loginPage.clickLogin()
     loginPage.verifyLoginFailed()
   })
 
   it('should not login with empty username', () => {
     loginPage.typePassword(Cypress.env('password'))
     loginPage.verifyLoginButtonDisabled()
-    loginPage.verifyLoginFailed()
   })
 
   it('should not login with empty password', () => {
     loginPage.typeUsername(Cypress.env('username'))
     loginPage.verifyLoginButtonDisabled()
-    loginPage.verifyLoginFailed()
   })
 
   it('should not login with empty credentials', () => {
     loginPage.verifyLoginButtonDisabled()
-    loginPage.verifyLoginFailed()
   })
 
 })
